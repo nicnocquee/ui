7ac24763292d345436e45f6364bba5dd58015ff6
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-11-08 11:50:18 +0100

    refactor layout: update grid columns in App component for better responsiveness and spacing on smaller screens

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { CenterAnimatedTextExample } from "./examples/center-animated-text";
 
 function App() {
   return (
-    <div className="max-w-7xl mx-auto my-12 p-8 grid grid-cols-2 gap-8">
+    <div className="max-w-7xl mx-auto my-12 p-8 grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-8">
       <BlueSkyFlutterExample />
       <CenterAnimatedTextExample />
     </div>
